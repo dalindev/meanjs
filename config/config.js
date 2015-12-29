7e39c9dc4fca2d62fe3d27fe0f50ac87906de089
JavaScript rule them ALL <hdltyty@163.com>
2015-12-29 15:02:18 -0500

    commit 5

--- config/config.js
+++ config/config.js
@@ -60,7 +60,7 @@ var validateEnvironmentVariable = function () {
     } else {
       console.error(chalk.red('+ Error: NODE_ENV is not defined! Using default development environment'));
     }
-    process.env.NODE_ENV = 'production';
+    process.env.NODE_ENV = 'development';
   }
   // Reset console color
   console.log(chalk.white(''));
