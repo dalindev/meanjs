70d7ece6c64f5b409423add49ca83d6c64e450f2
Dalin Huang <hdltyty@163.com>
2016-02-14 02:22:06 -0500

    Resume add to home page
    =D

--- config/env/development.js
+++ config/env/development.js
@@ -33,7 +33,7 @@ module.exports = {
     }
   },
   app: {
-    title: defaultEnvConfig.app.title + ' - Development Environment'
+    title: defaultEnvConfig.app.title + ' - Dalin - DEMO'
   },
   facebook: {
     clientID: process.env.FACEBOOK_ID || 'APP_ID',
