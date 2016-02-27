e0e7791185cc790ab5bcf0a414310ee6b7ea3bf2
Dalin Huang <hdltyty@163.com>
2016-02-26 22:48:45 -0500

    SEO 002 add keywords
    add keywords

--- config/env/default.js
+++ config/env/default.js
@@ -3,8 +3,8 @@
 module.exports = {
   app: {
     title: 'meanjs',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-    keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
+    description: 'appmean,heroku,herokuapp,appmean.herokuapp.com,Dalin Huang,Full-Stack,meanjs',
+    keywords: 'appmean,heroku,herokuapp,appmean.herokuapp.com,Dalin Huang,Full-Stack,meanjs,mongodb,express,angularjs,node.js,mongoose,passport',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
   port: process.env.PORT || 3000,
